c1ae96dae1d96256b4d463192c01c5265ef5689e
Aaron Burt <aburt@playhaven.com>
2015-12-17 15:18:21 -0800

    remove more guff from tcollector.py

--- bucky/tcollector.py
+++ bucky/tcollector.py
@@ -14,61 +14,24 @@
 #
 # Copyright 2011 Cloudant, Inc.
 
-import six
 import sys
 import time
-import socket
-import struct
 import logging
-try:
-    import cPickle as pickle
-except ImportError:
-    import pickle
 
 import bucky.client as client
 import bucky.names as names
 
 
-if six.PY3:
-    xrange = range
-
-
 log = logging.getLogger(__name__)
-
-
-class DebugSocket(object):
-    def sendall(self, data):
-        sys.stdout.write(data)
 
 
 class CarbonClient(client.Client):
     def __init__(self, cfg, pipe):
         super(CarbonClient, self).__init__(pipe)
         self.debug = cfg.debug
-        self.ip = cfg.graphite_ip
-        self.port = cfg.graphite_port
-        self.max_reconnects = cfg.graphite_max_reconnects
-        self.reconnect_delay = cfg.graphite_reconnect_delay
-        self.backoff_factor = cfg.graphite_backoff_factor
-        self.backoff_max = cfg.graphite_backoff_max
-        if self.max_reconnects <= 0:
-            self.max_reconnects = sys.maxint
-        self.connect()
-
-    def connect(self):
-        log.debug("Connected the debug socket.")
-        self.sock = DebugSocket()
-        return
-
-    def reconnect(self):
-        self.close()
-        self.connect()
 
     def close(self):
-        try:
-            self.sock.close()
-        except:
-            pass
+        pass
 
 
 class PlaintextClient(CarbonClient):
